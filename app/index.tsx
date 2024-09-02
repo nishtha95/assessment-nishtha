61a49ee5c86513fbbeaf142bc47e817ed4394c31
Nishtha <nishtha@admins-MacBook-Air.local>
2024-09-02 08:39:56 +0530

    mockapi data added

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,25 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, FlatList } from "react-native";
 
 export default function Index() {
+  const [homes, setHomes]= useState([]);
+  const[isLoading, setLoading]= useState(true);
+  useEffect(()=>{
+    const fetchHomes = async () => {
+      try {
+        const response = await fetch(`http://192.168.1.4:3000/homes`);
+        const data = await response.json();
+        setHomes(data);
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching homes:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchHomes();
+  },[]);
   return (
     <View
       style={{
@@ -9,7 +28,6 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
